a5aa3587ad97290b30dc6924dcad37d362274dfc
MaxCesni7 <c84max@hotmail.com>
2023-07-05 13:39:58 -0600

    Corrección de nombre de las clases de estilo para la renderización de productos y cambiados los valores de px a rem

--- js/cards.js
+++ js/cards.js
@@ -11,14 +11,14 @@ const plantas = [
     },
     {
         id: 2,
-        nombre: "CROTÓN",
+        nombre: "CROTÓ",
         url: "https://hips.hearstapps.com/hmg-prod/images/garden-croton-or-variegated-croton-euphorbiaceae-news-photo-1670849839.jpg?crop=1xw:1xh;center,top&resize=980:*",
         precio: 14.02,
         descripcion: "Lorem Ipsum is simply dummy tex"
     },
     {
         id: 3,
-        nombre: "HELECHO",
+        nombre: "HELECHO BOSTON otrosss",
         url: "https://hips.hearstapps.com/hmg-prod/images/helecho-natural-1631189259.png?crop=1xw:1xh;center,top&resize=980:*",
         precio: 14.02,
         descripcion: "Lorem Ipsum is simply dummy tex"
@@ -72,6 +72,133 @@ const plantas = [
         precio: 14.02,
         descripcion: "Lorem Ipsum is simply dummy tex"
     },
+    {
+        id: 11,
+        nombre: "Palma Areca",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/Palma-Areca-Gd.jpg?fit=1080%2C1080&ssl=1",
+        precio: 250.00,
+        descripcion: "planta de interior excelente para dar vida a tu sala",
+    },
+    {
+        id: 12,
+        nombre: "Muñeca",
+        url: "https://hagearbeider.com/wp-content/uploads/2021/02/1612526530_480_Omsorg-for-den-vakre-cordyline-tango.jpg",
+        precio: 240.00,
+        descripcion: "Planta de sombra da color a tus espacios con su tonalidad llamativa"
+    },
+    {
+        id: 13,
+        nombre: "Orquideas",
+        url: "https://editorialtelevisa.brightspotcdn.com/dims4/default/03eabb0/2147483647/strip/true/crop/1194x672+3+0/resize/1000x563!/quality/90/?url=https%3A%2F%2Fk2-prod-editorial-televisa.s3.amazonaws.com%2Fbrightspot%2Fwp-content%2Fuploads%2F2022%2F07%2Forquidea.jpg",
+        precio: 750.00,
+        descripcion: "Una planta que resalta por su elegancia y belleza excelente para mostrarla en tu casa o en tu oficina",
+    },
+    {
+        id: 14,
+        nombre: "Cala",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbKnbQkbQ3Mxz15xR8g-Qt3X4M4BsifaLfJA&usqp=CAU",
+        precio: 350.00,
+        descripcion: "Planta de semisombra con colores llamativos apta para tu coleccion"
+    },
+    {
+        id: 15,
+        nombre: "Anturio",
+        url: "https://www.elmueble.com/medio/2022/05/17/lilac-anturio_9317cc4d_674x674.jpeg",
+        precio: 550.00,
+        descripcion: "planta de interior con una variedad de colores es una opcion excelente para dar vida a tu hogar ",
+    },
+    {
+        id: 16,
+        nombre: "Violeta ",
+        url: "https://thumbs.dreamstime.com/b/flores-violetas-de-saintpaulia-podridas-plantar-en-maceta-y-herramientas-jard%C3%ADn-para-plantas-macetas-tablas-madera-198059583.jpg",
+        precio: 240.00,
+        descripcion: "Planta de luz interior una excelente opcion si lo que buscas es tener una planta resistente y con mucho color"
+    },
+    {
+        id: 1,
+        nombre: "Esfera Barro",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/Esfera-chica-barro.jpg?resize=300%2C300&ssl=1",
+        precio: 250.00,
+        descripcion: "Esta es una maceta que resalta mucho con tus plantas mas llamativas",
+    },
+    {
+        id: 2,
+        nombre: "Taza Barro",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/Taza-chica.jpg?resize=300%2C300&ssl=1",
+        precio: 200.00,
+        descripcion: "Una maceta minimalista para tus plantas"
+    },
+    {
+        id: 3,
+        nombre: "Esfera Ceramica",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/esfera-grande-fibra.jpg?resize=300%2C300&ssl=1",
+        precio: 450.00,
+        descripcion: "Una maceta perfecta para resaltar tus plantas ",
+    },
+    {
+        id: 4,
+        nombre: "Cubo Ceramica",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/cubo-grande-fibra.jpg?resize=300%2C300&ssl=1",
+        precio: 550.00,
+        descripcion: "Una maceta muy elegante para tus mejores plantas"
+    },
+    {
+        id: 5,
+        nombre: "Jardinera Barro",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/Jardinera-Barro-1.jpg?resize=300%2C300&ssl=1",
+        precio: 250.00,
+        descripcion: "Jardinera de barro para los minimalistas",
+    },
+    {
+        id: 6,
+        nombre: "Jardinera Fibra de Vidrio ",
+        url: "https://i0.wp.com/plantify.mx/wp-content/uploads/2021/10/jardinera-fibra.jpg?resize=600%2C600&ssl=1",
+        precio: 530.00,
+        descripcion: "Jardinera elaborada con fibra de vidrio color negro"
+    },
+    {
+        id: 1,
+        nombre: "Fertilizante Nitrofosca 1kg",
+        url: "https://elheraldodemartinez.com.mx/images/Articulos2018/Articulo/2020/011Noviembre/28Nov/PAG-3-PRINCIPAL.png",
+        precio: 250.00,
+        descripcion: "Este fertilizante ayudara a que tu planta crezca y tenga brotes nuevos",
+    },
+    {
+        id: 2,
+        nombre: "Fertilizante Florifil 1kg",
+        url: "https://agropos.com.br/wp-content/uploads/2022/03/Fertilizante-Para-Planta.jpg",
+        precio: 250.00,
+        descripcion: "Este fertilizante ayudara a que tus plantas que dan flores tengan aun mas"
+    },
+    {
+        id: 3,
+        nombre: "Sistema de Reigo",
+        url: "https://m.media-amazon.com/images/I/812q2D0i3sL._AC_UF1000,1000_QL80_.jpg",
+        precio: 950.00,
+        descripcion: "Es un sistema que te permitira regar de manera facil y sencilla hasta 30m de distancia",
+    },
+    {
+        id: 4,
+        nombre: "Kit de Herramientas",
+        url: "https://as2.ftcdn.net/v2/jpg/02/65/10/51/1000_F_265105182_1kHISR3wzXYeADjOmnhU8lLvd6EyNxuE.jpg",
+        precio: 550.00,
+        descripcion: "Todo lo que necesitas para dar mantenimiento a tus preciadas plantas"
+    },
+    {
+        id: 5,
+        nombre: "Cinturon de Cuero",
+        url: "https://www.elblogdelatabla.com/wp-content/uploads/2020/11/cinturon-jardineria-herramientas-mano-florista-cuero-merifaLeather.jpg",
+        precio: 450.00,
+        descripcion: "Vuelve mas facil tu trabajo en la jardineria con ayuda de este cinturon perecto para tus herramientas",
+    },
+    {
+        id: 6,
+        nombre: "Manguera de Riego ",
+        url: "https://plasticforte.com/wp-content/uploads/2022/06/accesorios-jardineria-plasticforte.jpg",
+        precio: 500.00,
+        descripcion: "Para que tus plantas queden perfectamente regadas utiliza nuestra manguera con soporte a pared"
+    },
+   
     
     
 ]
@@ -94,8 +221,8 @@ const createCard = () => {
     const card = document.createElement('div');
     const imgContainer = document.createElement('div');
     const infoContainer = document.createElement('div');
-    card.classList.add('col-sm-4', 'card', 'e1_2');
-    imgContainer.classList.add('e1_7');
+    card.classList.add('col-sm-4', 'card', 'tarjetaContenedor');
+    imgContainer.classList.add('imagenProducto');
     card.append(imgContainer, infoContainer);
     return card
 }
@@ -118,10 +245,10 @@ const populateElements = (infoPlantas, planta) => {
     infoPlantas.precio.textContent = "$"+planta.precio+" / pesos";
     infoPlantas.button.textContent = "Al Carrito";
 
-    infoPlantas.nombre.classList.add("e1_4");
-    infoPlantas.precio.classList.add("e1_5");
-    infoPlantas.descripcion.classList.add("e1_6");
-    infoPlantas.button.classList.add("e2_8");
+    infoPlantas.nombre.classList.add("nombreProducto");
+    infoPlantas.precio.classList.add("precio");
+    infoPlantas.descripcion.classList.add("decripcion");
+    infoPlantas.button.classList.add("botonTarjeta");
 
     return infoPlantas;
 }
