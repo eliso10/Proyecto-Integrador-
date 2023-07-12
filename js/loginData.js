57eed48a203ea2e9ad1cc9e4162fff41dd73eebf
bergmani <bergman.ijp@gmail.com>
2023-07-11 18:08:13 -0600

    se cambian verificaciones de bootstrap por alert y se añade regex por medio de js

--- js/loginData.js
+++ js/loginData.js
@@ -1,38 +1,48 @@
-// const regexEmail = /^[\w.+\-]+@gmail\.com$/;
-// const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+const inputEmail = document.getElementById('floatingInputEmail');
+const inputPassword = document.getElementById('floatingPassword');
+const form = document.getElementById('logIn');
+const loginButton = document.getElementById('mainButton');
 
+//regex
+const regexEmail = /^[\w.+\-]+@gmail\.com$/;
+const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
-(() => {
-    'use strict'
+    //poblar alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+const wrapper = document.createElement('div')
+wrapper.innerHTML = [
+  `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+  `   <div>${message}</div>`,
+  '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+  '</div>'
+].join('')
+
+alertPlaceholder.append(wrapper)
+}
+
+//revision de los campos con el regex
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  if(regexPassword.test(inputPassword.value) && regexEmail.test(inputEmail.value)){
+    //data en JSON object si es coorecta la informacion
+    const data = new FormData(event.target);
+    const allData = Object.fromEntries(data.entries());
+
+    //se muestra en consola el objeto json
+    console.log(JSON.stringify(allData));
+
+    //Enviar a la pagina de inicio si se inicia sesion exitosamente
+    loginButton.addEventListener('click', function() {
+      window.location.href = '../index.html';
+  });
+
+  } else if(!regexPassword.test(inputPassword.value) && !regexEmail.test(inputEmail.value)){
     
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-    let inputEmail = document.getElementById('floatingInputEmail');
-    let inputPassword = document.getElementById('floatingPassword');
-
-    // inputEmail.addEventListener('input', input => {
-    //     regexEmail.test(input.target.value)
-    //     ?   
-    // })
-    
-    
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-        } else{
-        
-        
-        const data = new FormData(event.target);
-        const allData = Object.fromEntries(data.entries());
-
-        console.log(JSON.stringify(allData));
-        }
-        
-
-        form.classList.add('was-validated')
-        }, false)
-    })
-    })()
+    appendAlert('Verifica la información de ambos campos', 'danger');
+  } else if (!regexEmail.test(inputEmail.value)){
+    appendAlert('Verifica la información del correo electrónico', 'danger');
+  } else{
+    appendAlert('Verifica la información de la contraseña. Recuerda que son mínimo 8 caracteres: al menos un número, un símbolo, una mayúscula y una minúscula', 'danger'); 
+  }
+})
