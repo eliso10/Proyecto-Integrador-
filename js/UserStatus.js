17747c327f2984c0c195573b4c3f6edb26486ad2
MaxCesni7 <c84max@hotmail.com>
2023-07-27 11:25:54 -0600

    SCRIPT DE STATUS DE USUARIO

---
+++ js/UserStatus.js
@@ -0,0 +1,24 @@
+const usuarioEstatus = document.getElementById('Usuario-Estatus');
+
+localStorage.setItem('UserStatus', false);
+
+
+
+function actualizacion () {
+    let ActualizacionEstatus = JSON.parse(localStorage.getItem('UserStatus'));
+    console.log("Elemento del local Store: "+JSON.parse(localStorage.getItem('UserStatus')));
+    console.log(usuarioEstatus);
+
+    if(ActualizacionEstatus){
+        usuarioEstatus.innerHTML = '<a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">Usuario</a><ul class="dropdown-menu"><li id="Primer-Estatus"><a class="dropdown-item" href="#">Perfil</a></li><li class="Segundo-Estatus"><a class="dropdown-item" href="#" onclick="CerrarSesion()">Cerrar Sesion</a></li></ul>'; 
+    } 
+}
+
+actualizacion();
+
+function CerrarSesion(){
+    localStorage.setItem('UserStatus', "false");
+    console.log(JSON.parse(localStorage.getItem('UserStatus')));
+    window.location.href = '../index.html';
+}
+
